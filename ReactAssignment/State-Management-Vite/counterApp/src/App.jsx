0f5bf40465ba396c3187340c1fe04878a74fafb2
Dharmender <dharmender7077@gmail.com>
2024-02-13 10:03:05 +0530

    Counter with Vite

---
+++ ReactAssignment/State-Management-Vite/counterApp/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Counter from './Component/Counter'
+function App() {
+
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ ReactAssignment/State-Management-Vite/counterApp/src/Component/Counter.jsx
@@ -0,0 +1,15 @@
+// import React from "react"
+import { useState } from "react"
+const Counter = () => {
+const [count , setCount] = useState(0)
+
+  return (
+    <div>
+        <h1>{count}</h1>
+        <button onClick={()=> setCount(count + 1)}>Increase by 1</button>
+        <button onClick={()=> setCount(count - 1)}>Decrease by 1</button>
+    </div>
+  )
+}
+
+export default Counter
